6bc88c95156eb8500a8423d80a1efec303b96673
nazmulcuet11 <nazmulcuet11@gmail.com>
2021-04-03 22:40:17 +0600

    Implement sticky headers

--- CustomCollectionViewLayout/StickyHeaders/Layouts/StickyHeadersLayout.swift
+++ CustomCollectionViewLayout/StickyHeaders/Layouts/StickyHeadersLayout.swift
@@ -11,5 +11,58 @@ class StickyHeadersLayout: UICollectionViewFlowLayout {
     let numberOfColums = 3
     let padding: CGFloat = 2.0
 
-    
+    override func shouldInvalidateLayout(
+        forBoundsChange newBounds: CGRect
+    ) -> Bool {
+        return true
+    }
+
+    override func layoutAttributesForElements(
+        in rect: CGRect
+    ) -> [UICollectionViewLayoutAttributes]? {
+        guard let attributesList = super.layoutAttributesForElements(in: rect),
+              let collectionView = collectionView
+        else {
+            return []
+        }
+
+        for attributes in attributesList {
+            guard let kind = attributes.representedElementKind,
+                  kind == UICollectionView.elementKindSectionHeader
+            else {
+                continue
+            }
+
+            let section = attributes.indexPath.section
+            
+            let numberOfItems = collectionView.numberOfItems(inSection: section)
+
+            guard numberOfItems > 0 else {
+                continue
+            }
+
+            let firstIndexPath = IndexPath(row: 0, section: section)
+            let lastIndexPath = IndexPath(row: numberOfItems - 1, section: section)
+
+            guard let attributesForFirstItemInSection = layoutAttributesForItem(at: firstIndexPath),
+                  let attributesForLastItemInSection = layoutAttributesForItem(at: lastIndexPath)
+            else {
+                continue
+            }
+
+            let offsetY = collectionView.contentOffset.y + collectionView.adjustedContentInset.top
+            var adjustedFrame = attributes.frame
+
+            let minY = attributesForFirstItemInSection.frame.minY - adjustedFrame.height - padding
+            let maxY = attributesForLastItemInSection.frame.maxY - adjustedFrame.height + padding
+
+            let y = min(maxY, max(minY, offsetY))
+            adjustedFrame.origin.y = y
+
+            attributes.frame = adjustedFrame
+            attributes.zIndex = 99
+        }
+
+        return attributesList
+    }
 }
